26f285c513d19bed8920e04a4272120b5948c62d
Carlos de las Heras <cahersan@gmail.com>
2026-09-23 15:14:33 +0200

    Updated .gitignore

--- piedra.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-echo "piedra.service: ## Starting ##" | systemd-cat -p info
-
-cd /home/carlos/club_de_piedras/
-source env/bin/activate
-python main.py
-
-echo "piedra.service: ## Started ##" | systemd-cat -p info
